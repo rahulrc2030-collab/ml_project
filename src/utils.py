8de56cd0b57d1fbf4979f041fbe3539dbcfe7819
rahul <rahulrc2030@gmail.com>
2026-09-18 06:58:12 +0530

    application

--- src/utils.py
+++ src/utils.py
@@ -65,5 +65,11 @@ def evaluate_models(x_train,y_train,x_test,y_test,models,diff_param):
         raise CustomException(e,sys)
 
     
+def load_obj(file_path):
+    try:
+        with open(file_path,"rb") as file:
+            return dill.load(file)
+    except Exception as e:
+        raise CustomException(e,sys)
 
 
